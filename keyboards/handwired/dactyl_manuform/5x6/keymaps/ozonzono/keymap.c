69192956185a8253af8f27a7b66292fe0d9216bb
Liad Oz <liadozil@gmail.com>
2022-11-13 19:20:46 +0200

    changed dactly keymap

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/ozonzono/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/ozonzono/keymap.c
@@ -16,22 +16,22 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-        KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
+        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_MINS,
+        KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_BSPC,
         KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
         KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                          KC_LBRC,KC_RBRC,                                                       KC_BSLASH, KC_EQL,
-                                         KC_LCTL ,KC_ENT,                        KC_SPC, KC_LSTRT,
-                                         KC_LGUI, RAISE,                         LOWER,  KC_LEND,
-                                         KC_LALT, KC_LEAD,                       KC_LEAD, KC_LALT
+                                         KC_LCTL ,KC_ENT,                        KC_SPC  ,LOWER  ,
+                                         KC_LGUI, RAISE,                         KC_LEAD ,KC_LGUI,
+                                         KC_LALT, KC_LEAD,                       KC_LSTRT,KC_LEND
     ),
 
     [_LOWER] = LAYOUT_5x6(
-        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_DEL,
-        _______,_______,_______,_______,_______,KC_LBRC,                        KC_PGUP, KC_PRVWD , KC_UP , KC_NXTWD ,_______,KC_PLUS,
-        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,KC_LPRN,                        KC_PGDN, KC_LEFT , KC_DOWN , KC_RGHT ,KC_MINS,KC_PIPE,
-        _______,_______,_______,_______,_______,_______,                        _______, KC_LSTRT , KC_P2 , KC_LEND ,KC_EQL ,KC_UNDS,
-                                                _______,KC_PSCR,            _______, KC_P0,
+        KC_TILD,KC_EXLM, KC_AT ,KC_HASH,KC_DLR ,KC_PERC,                        KC_CIRC, KC_AMPR ,KC_ASTR,KC_LPRN ,KC_RPRN,_______,
+        _______,_______,_______,_______,_______,_______,                        KC_PGUP, KC_PRVWD, KC_UP ,KC_NXTWD,KC_PSCR,KC_DEL ,
+        _______,KC_HOME,KC_PGUP,KC_PGDN,KC_END ,_______,                        KC_PGDN, KC_LEFT ,KC_DOWN,KC_RGHT ,KC_MINS,KC_PIPE,
+        _______,_______,_______,_______,_______,_______,                        _______, KC_LSTRT,_______,KC_LEND ,_______,_______,
+                        _______,_______,                                                            _______,_______,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______,
                                                 _______,_______,            _______,_______
@@ -40,13 +40,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_5x6(
           KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
-          CG_TOGG,KC_VOLD,KC_MUTE,KC_VOLU, RESET ,KC_LBRC,                        KC_RBRC,_______,KC_NLCK,KC_INS ,KC_SLCK,KC_MUTE,
-          _______,KC_LEFT,KC_UP  ,KC_DOWN,KC_RGHT,KC_LPRN,                        KC_RPRN,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_VOLU,
-          _______,_______,_______,KC_CAPS,_______,_______,                        _______,_______,_______,_______,_______,KC_VOLD,
-                                                  _______,_______,            KC_EQL ,_______,
+          CG_TOGG,_______,KC_VOLD,KC_MUTE,KC_VOLU,KC_LBRC,                        _______,_______,KC_INS ,_______,_______,_______,
+          _______,_______,KC_TAB ,KC_LALT,KC_ENT ,KC_CAPS,                        _______,KC_MPRV,KC_MPLY,KC_MNXT,_______,KC_PIPE,
+          _______,_______,_______,_______,KC_LCBR,KC_RCBR,                        KC_LBRC,KC_RBRC,_______,_______,_______,_______,
+                          _______,_______,                                                        _______,_______,
                                                   _______,_______,            _______,_______,
                                                   _______,_______,            _______,_______,
-                                                  _______,_______,            _______,_______
+                                                  _______, RESET ,            _______,_______
     )
 };
 
